e1afc5aee0f3d13ae685e683c518d128d7caf944
sbakkali01 <bakkalisami01@gmail.com>
2025-03-09 19:11:07 +0100

    created the first two classes

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Elevator:
+    min_floor=0
+    max_floor=4
+    current_floor=None
+    # capacity=1
+
+    def __init__(self):
+        self.current_floor=0
+
+class Person:
+    current_floor=None
+    direction:int=None
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('PyCharm')
 
